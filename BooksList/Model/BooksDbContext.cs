5903a4871cd6af4361ae6dd7cc0915ea53c32f86
Nhlelo <nhlelow@gmail.com>
2020-06-16 15:28:48 +0200

    Add Book Model and Database Configurations

---
+++ BooksList/Model/BooksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksList.Model
+{
+    public class BooksDbContext:DbContext
+    {
+        public BooksDbContext(DbContextOptions<BooksDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Book> Book { get; set; }
+
+    }
+}
--- BooksList/Pages/Index.cshtml.cs
+++ BooksList/Pages/Index.cshtml.cs
@@ -21,5 +21,6 @@ namespace BooksList.Pages
         {
 
         }
+        
     }
 }
