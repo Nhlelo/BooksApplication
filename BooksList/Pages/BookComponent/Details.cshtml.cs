e1eef73c10c0c1534e8fc4e909b99f252180008b
Nhlelo <nhlelow@gmail.com>
2020-06-16 16:45:43 +0200

    Add crud razor pages

---
+++ BooksList/Pages/BookComponent/Details.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BooksList.Model;
+
+namespace BooksList.Pages.BookComponent
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly BooksList.Model.BooksDbContext _context;
+
+        public DetailsModel(BooksList.Model.BooksDbContext context)
+        {
+            _context = context;
+        }
+
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Book = await _context.Book.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ BooksList/Pages/BookComponent/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BooksList.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksList.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly BooksDbContext _db;
+        public IndexModel(BooksDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
+        }
+    }
+}
+
